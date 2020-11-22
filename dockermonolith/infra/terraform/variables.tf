1312a9f29205196b57bd7878c278f0245e5b8677
Kirill Dobrynin <kdobrynin@astralinux.ru>
2020-11-22 17:39:26 +0300

    Add terraform configs

---
+++ dockermonolith/infra/terraform/variables.tf
@@ -0,0 +1,29 @@
+  
+variable cloud_id {
+    description = "Cloud"
+}
+
+variable folder_id {
+    description = "Folder"
+}
+
+variable zone {
+    description = "Zone"
+    default = "ru-central1-a"
+}
+
+variable host_count {
+    description = "Host count"
+}
+
+variable public_key_path {
+    description = "Path to public key used for ssh access"
+}
+
+variable subnet_id {
+    description = "Subnet"
+}
+
+variable image_id {
+    description = "VM image"
+}
